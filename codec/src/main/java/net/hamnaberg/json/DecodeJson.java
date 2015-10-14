dbf7f03ebfb62361468a7c8306645d8e091eb759
Erlend Hamnaberg <erlend@hamnaberg.net>
2015-10-14 19:30:04 +0200

    nullable in decode / encoder

--- codec/src/main/java/net/hamnaberg/json/DecodeJson.java
+++ codec/src/main/java/net/hamnaberg/json/DecodeJson.java
@@ -4,4 +4,8 @@ import java.util.Optional;
 
 public interface DecodeJson<A> {
     Optional<A> fromJson(Json.JValue value);
+
+    default A fromJsonUnsafe(Json.JValue value) {
+        return fromJson(value).orElse(null);
+    }
 }
--- codec/src/main/java/net/hamnaberg/json/EncodeJson.java
+++ codec/src/main/java/net/hamnaberg/json/EncodeJson.java
@@ -4,4 +4,8 @@ import java.util.Optional;
 
 public interface EncodeJson<A> {
     Optional<Json.JValue> toJson(A value);
+
+    default Json.JValue toJsonUnsafe(A value) {
+        return toJson(value).orElse(null);
+    }
 }
