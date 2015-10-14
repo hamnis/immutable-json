5dd2502f2b2eaf81fee22e75bc2e4bba02fd427f
Erlend Hamnaberg <erlend@hamnaberg.net>
2015-10-14 17:38:33 +0200

    Test

---
+++ codec/src/test/java/net/hamnaberg/json/PersonCodecTest.java
@@ -0,0 +1,75 @@
+package net.hamnaberg.json;
+
+import javaslang.Tuple2;
+import org.junit.Test;
+
+import java.util.LinkedHashMap;
+import java.util.Optional;
+import java.util.function.Function;
+
+import static org.junit.Assert.*;
+
+public class PersonCodecTest {
+    private static class Person {
+        public final String name;
+        public final int age;
+
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Person person = (Person) o;
+
+            if (age != person.age) return false;
+            return name.equals(person.name);
+
+        }
+
+        @Override
+        public int hashCode() {
+            int result = name.hashCode();
+            result = 31 * result + age;
+            return result;
+        }
+    }
+
+    private enum PersonIso implements Iso<Person, Tuple2<String, Integer>> {
+        INSTANCE;
+
+        @Override
+        public Function<Tuple2<String, Integer>, Person> reverseGet() {
+            return t -> new Person(t._1, t._2);
+        }
+
+        @Override
+        public Function<Person, Tuple2<String, Integer>> get() {
+            return t -> new Tuple2<>(t.name, t.age);
+        }
+    }
+
+
+    @Test
+    public void personCreate() {
+        Json.JValue value = Json.jObject(new LinkedHashMap<String, Json.JValue>(){{
+                put("name", Json.jString("Erlend Hamnaberg"));
+                put("age", Json.jNumber(34));
+        }});
+
+        JsonCodec<Person> codec = Codecs.codec2(PersonIso.INSTANCE, Codecs.StringCodec, Codecs.intCodec).apply("name", "age");
+
+
+        Person person = new Person("Erlend Hamnaberg", 34);
+        Optional<Person> personOpt = codec.fromJson(value);
+        assertTrue(personOpt.isPresent());
+        assertEquals(person, personOpt.get());
+        Optional<Json.JValue> jsonOpt = codec.toJson(person);
+        assertTrue(jsonOpt.isPresent());
+        assertEquals(value, jsonOpt.get());
+    }
+}
