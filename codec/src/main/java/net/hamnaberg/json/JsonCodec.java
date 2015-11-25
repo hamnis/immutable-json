61b3b4eefc5d98902c2310342680b768e8aad832
Erlend Hamnaberg <erlend@hamnaberg.net>
2015-11-25 17:02:58 +0100

    Narrow and tryNarrow

--- codec/src/main/java/net/hamnaberg/json/JsonCodec.java
+++ codec/src/main/java/net/hamnaberg/json/JsonCodec.java
@@ -1,21 +1,42 @@
 package net.hamnaberg.json;
 
+import javaslang.control.Try;
+
 import java.util.Optional;
 import java.util.function.Function;
 
 public interface JsonCodec<A> extends EncodeJson<A>, DecodeJson<A> {
-    default <B> JsonCodec<B> xmap(Function<A, B> f1, Function<B, A> f2) {
+    default <B> JsonCodec<B> xmap(Function<A, B> f, Function<B, A> g) {
         JsonCodec<A> that = this;
         return new JsonCodec<B>() {
             @Override
             public Optional<B> fromJson(Json.JValue value) {
-                return that.fromJson(value).map(f1);
+                return that.fromJson(value).map(f);
             }
 
             @Override
             public Optional<Json.JValue> toJson(B value) {
-                return that.toJson(f2.apply(value));
+                return that.toJson(g.apply(value));
             }
         };
+    }
+
+    default <B> JsonCodec<B> narrow(Function<A, Try<B>> f, Function<B, A> g) {
+        JsonCodec<A> that = this;
+        return new JsonCodec<B>() {
+            @Override
+            public Optional<B> fromJson(Json.JValue value) {
+                return that.fromJson(value).map(f).flatMap(Try::toJavaOptional);
+            }
+
+            @Override
+            public Optional<Json.JValue> toJson(B value) {
+                return that.toJson(g.apply(value));
+            }
+        };
+    }
+
+    default <B> JsonCodec<B> tryNarrow(Function<A, B> f, Function<B, A> g) {
+        return narrow(a -> Try.of(() -> f.apply(a)), g);
     }
 }
