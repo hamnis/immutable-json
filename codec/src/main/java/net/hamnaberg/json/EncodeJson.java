6fc16f06dca906cddb67ecd0b54395077912551f
Erlend Hamnaberg <erlend@hamnaberg.net>
2016-02-16 10:38:45 +0100

    contramap on EncodeJson

--- codec/src/main/java/net/hamnaberg/json/EncodeJson.java
+++ codec/src/main/java/net/hamnaberg/json/EncodeJson.java
@@ -1,11 +1,16 @@
 package net.hamnaberg.json;
 
 import java.util.Optional;
+import java.util.function.Function;
 
 public interface EncodeJson<A> {
     Optional<Json.JValue> toJson(A value);
 
     default Json.JValue toJsonUnsafe(A value) {
         return toJson(value).orElse(null);
+    }
+
+    default <B> EncodeJson<B> contramap(Function<B, A> f) {
+        return value -> this.toJson(f.apply(value));
     }
 }
