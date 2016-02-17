80a118dc10ddc28d1c547a82a9d571629bbb9209
Erlend Hamnaberg <erlend@hamnaberg.net>
2016-02-17 18:38:50 +0100

    Functional interface annotation

--- codec/src/main/java/net/hamnaberg/json/extract/Extractor.java
+++ codec/src/main/java/net/hamnaberg/json/extract/Extractor.java
@@ -5,5 +5,6 @@ import net.hamnaberg.json.Json;
 import java.util.Optional;
 import java.util.function.Function;
 
+@FunctionalInterface
 public interface Extractor<A> extends Function<Json.JObject, Optional<A>> {
 }
