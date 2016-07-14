5893eb1a5c1f6d0cb030619a3a8b3f00391d3a8c
Erlend Hamnaberg <erlend@hamnaberg.net>
2016-07-14 12:16:54 +0200

    use utf-8

--- core/src/main/java/net/hamnaberg/json/io/IOUtils.java
+++ core/src/main/java/net/hamnaberg/json/io/IOUtils.java
@@ -4,14 +4,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.nio.charset.StandardCharsets;
 
-/**
- * Created by erlhamna on 19/02/16.
- */
 public class IOUtils {
 
     public static String toString(InputStream stream) throws IOException {
-        return toString(new InputStreamReader(stream));
+        return toString(new InputStreamReader(stream, StandardCharsets.UTF_8));
     }
 
     public static String toString(Reader reader) throws IOException {
@@ -19,7 +17,7 @@ public class IOUtils {
         StringBuilder buffer = new StringBuilder();
         int numCharsRead;
         try(Reader r = reader) {
-            while ((numCharsRead = reader.read(arr, 0, arr.length)) != -1) {
+            while ((numCharsRead = r.read(arr, 0, arr.length)) != -1) {
                 buffer.append(arr, 0, numCharsRead);
             }
         }
