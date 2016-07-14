d621d5f6081df053481e38d7e6af22a413109980
Erlend Hamnaberg <erlend@hamnaberg.net>
2016-07-14 12:02:28 +0200

    Remove unused imports.

--- pointer/src/test/java/net/hamnaberg/json/pointer/RFCJsonPointerTest.java
+++ pointer/src/test/java/net/hamnaberg/json/pointer/RFCJsonPointerTest.java
@@ -4,9 +4,6 @@ import javaslang.control.Option;
 import net.hamnaberg.json.*;
 import org.junit.Test;
 
-import java.util.AbstractMap;
-import java.util.Map;
-import java.util.Optional;
 
 import static org.junit.Assert.*;
 
