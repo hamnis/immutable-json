c4a8ac9e9716b7e348b7f306d5315c62ef3f7bca
Erlend Hamnaberg <erlend@hamnaberg.net>
2016-08-15 09:20:56 +0200

    Compose and reverse.

--- codec/src/main/java/net/hamnaberg/json/Iso.java
+++ codec/src/main/java/net/hamnaberg/json/Iso.java
@@ -4,6 +4,35 @@ public interface Iso<A, B> {
     A reverseGet(B b);
     B get(A a);
 
+    default Iso<B, A> reverse() {
+        Iso<A, B> that = this;
+        return new Iso<B, A>() {
+            @Override
+            public B reverseGet(A a) {
+                return that.get(a);
+            }
+
+            @Override
+            public A get(B b) {
+                return that.reverseGet(b);
+            }
+        };
+    }
+
+    default <C> Iso<A, C> compose(Iso<B, C> iso) {
+        Iso<A, B> that = this;
+        return new Iso<A, C>() {
+            @Override
+            public A reverseGet(C c) {
+                return that.reverseGet(iso.reverseGet(c));
+            }
+
+            @Override
+            public C get(A a) {
+                return iso.get(that.get(a));
+            }
+        };
+    }
 
     static <A> Iso<A, A> identity() {
         return new IdIso<>();
