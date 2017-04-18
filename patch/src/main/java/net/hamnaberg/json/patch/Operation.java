0fc3ac3315ffaf7224c51f2c6ea154c1414f7ae4
Erlend Hamnaberg <erlend@hamnaberg.net>
2017-04-18 10:54:47 +0200

    Bump libs

--- patch/src/main/java/net/hamnaberg/json/patch/Operation.java
+++ patch/src/main/java/net/hamnaberg/json/patch/Operation.java
@@ -8,7 +8,7 @@ import java.util.EnumSet;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-public class Operation {
+class Operation {
     enum Op {
         Add("add"),
         Remove("remove"),
