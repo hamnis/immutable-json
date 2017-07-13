144cf9d5420d3e5edf70c186347cac696019ad66
Erlend Hamnaberg <erlend@hamnaberg.net>
2017-07-13 22:10:21 +0200

    More tests

--- codec/src/test/java/net/hamnaberg/json/codec/DecodeResultTest.java
+++ codec/src/test/java/net/hamnaberg/json/codec/DecodeResultTest.java
@@ -1,7 +1,11 @@
 package net.hamnaberg.json.codec;
 
+import io.vavr.Predicates;
 import io.vavr.collection.List;
 import org.junit.Test;
+
+import java.util.NoSuchElementException;
+import java.util.function.Predicate;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -24,5 +28,38 @@ public class DecodeResultTest {
         DecodeResult<String> res2 = DecodeResult.ok("Hello");
         DecodeResult<List<String>> sequenced = DecodeResult.sequence(List.of(res1, res2));
         sequenced.consume(f -> assertEquals("One or more results failed: Unable to decode [this]", f), err -> fail("success where expected fail"));
+    }
+
+
+    @Test
+    public void filter() throws Exception {
+        DecodeResult<String> res2 = DecodeResult.ok("Hello");
+        assertEquals("Hello", res2.filter(a -> a.equals("Hello")).unsafeGet());
+        assertEquals("Hello", res2.filter(Predicate.isEqual("Hello")).unsafeGet());
+        assertEquals("Hello", res2.filter(Predicates.isNotNull()).unsafeGet());
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void filterFail() throws Exception {
+        DecodeResult<String> res2 = DecodeResult.ok("Hello");
+        res2.filter("Goodbye"::equals).unsafeGet();
+    }
+
+    @Test
+    public void filterFailCustomError() throws Exception {
+        DecodeResult<String> res2 = DecodeResult.ok("Hello");
+        res2.filter("Goodbye"::equals, () -> "Nope").consume(a -> assertEquals("Nope", a), a -> fail("Succeeded when expected fail"));
+    }
+
+    @Test
+    public void toEither() throws Exception {
+        DecodeResult<String> res2 = DecodeResult.ok("Hello");
+        assertEquals("Hello", res2.toEither().get());
+    }
+
+    @Test
+    public void toJavaOptional() throws Exception {
+        DecodeResult<String> res2 = DecodeResult.ok("Hello");
+        assertEquals("Hello", res2.toJavaOptional().get());
     }
 }
